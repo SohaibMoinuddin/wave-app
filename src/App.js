95130f8453987ee455e345d8584447d837169f82
Sohaib Moinuddin <sohaiba@live.com>
2022-01-01 00:26:00 -0800

    check if wallet found

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import * as React from "react";
+import React, { useEffect } from "react";
 import { ethers } from "ethers";
 import "./App.css";
 
 export default function App() {
   const wave = () => {};
+
+  const checkIfWalletIsConnected = () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have Metamask!");
+      return;
+    } else {
+      console.log("We have the ethereum object!", ethereum);
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
 
   return (
     <div className="mainContainer">
